b86d1971585f95cf06b088572da1aa8e79e1860d
55 ОМ <230895@astanait.edu.kz>
2024-04-26 02:51:48 +0600

    commit

--- src/Task4.java
+++ src/Task4.java
@@ -1,31 +1,40 @@
 import java.util.*;
 
 public class Task4 {
-    public static void reverseArray(ArrayList<Integer> arr) {
-        int start = 0;
-        int end = arr.size() - 1;
-        while (start < end) {
-            int temp = arr.get(start);
-            arr.set(start, arr.get(end));
-            arr.set(end, temp);
-            start++;
-            end--;
+    public static void reverseStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int top = stack.pop();
+            reverseStack(stack);
+            insertAtBottom(stack, top);
+        }
+    }
+
+    private static void insertAtBottom(Stack<Integer> stack, int item) {
+        if (stack.isEmpty()) {
+            stack.push(item);
+        } else {
+            int top = stack.pop();
+            insertAtBottom(stack, item);
+            stack.push(top);
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
 
-        System.out.print("Enter the number of an array; ");
+        System.out.print("Enter the number of elements in the stack: ");
         int n = scanner.nextInt();
-        System.out.print("Enter values for the array:");
+        System.out.print("Enter values for the stack: ");
 
         for (int i = 0; i < n; i++) {
-            arr.add(scanner.nextInt());
+            stack.push(scanner.nextInt());
         }
 
-        reverseArray(arr);
-        System.out.println("Reversed array: " + arr);
+        reverseStack(stack);
+        System.out.print("Reversed stack: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
     }
 }
