ea59119e7ee924e40eab6f8df5e0954d70c4108b
55 ОМ <230895@astanait.edu.kz>
2024-04-25 23:25:01 +0600

    commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,25 +1,29 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Task3 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        Deque<Integer> shelf = new LinkedList<>();
 
-        for (int i = 0; i < N; i++) {
-            int operationType = scanner.nextInt();
-            if (operationType == 1) {
-                int diskNumber = scanner.nextInt();
-                shelf.addFirst(diskNumber - 1); //left
-            } else if (operationType == 2) {
-                int diskNumber = scanner.nextInt();
-                shelf.addLast(diskNumber - 1); //right
-            } else if (operationType == 3) {
-                int removedDisk = shelf.pollFirst(); //remove from left
-                System.out.println("3 " + removedDisk);
-            } else if (operationType == 4) {
-                int removedDisk = shelf.pollLast(); //remove from right
-                System.out.println("4 " + removedDisk);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Integer> liniya = new ArrayList<>();
+
+        int n = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            int q = input.nextInt();
+            if (q == 1) {
+                int disk = input.nextInt();
+                liniya.add(0, disk);  // add left
+            } else if (q == 2) {
+                int disk = input.nextInt();
+                liniya.add(disk);  // add right
+            } else if (q == 3) {
+                System.out.print(liniya.remove(0) + " ");  // minus left and print
+            } else if (q == 4) {
+                System.out.print(liniya.remove(liniya.size() - 1) + " ");  // minus right and print
+            } else {
+                // error print
+                System.out.println("Invalid operation");
             }
         }
     }
